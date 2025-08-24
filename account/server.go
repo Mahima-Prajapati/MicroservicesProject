e3ef5b66580d20a9b0d4af33cbea9cd483e8f75b
Mahima-Prajapati <118904414+Mahima-Prajapati@users.noreply.github.com>
2025-08-24 16:44:22 +0000

    Implemented Account Microservice

---
+++ account/server.go
@@ -0,0 +1,64 @@
+package account
+
+import (
+	"context"
+	"fmt"
+	"net"
+)
+
+type grpcServer struct {
+	service Service
+}
+
+func ListenGRPC(s Service, port int) error {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return err
+	}
+	
+	serv := grpc.NewServer()
+	pb.(serv,)
+	reflection.Register(serv)
+	return serv.Serve(lis)
+}
+
+func (s *grpcServer) PostAccount(ctx context.Context, r *pb.PostAccountRequest) (*pb.PostAccountResponse, error) {
+	acct, err := s.service.PostAccount(ctx, r.Name)
+	if err != nil {
+		return nil, err
+	}
+	
+	return &pb.PostAccountResponse{Account: &pb.Account{
+		Id: acct.ID,
+		Name: acct.Name,
+	}}, nil
+}
+
+func (s *grpcServer) GetAccount(ctx context.Context, r *pb.GetAccountRequest) (*pb.GetAccountResponse, error) {
+	acct, err := s.service.GetAccount(ctx, r.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.GetAccountResponse{Account: &pb.Account{
+		Id: acct.ID,
+		Name: acct.Name,
+	}}, nil
+}
+
+func (s *grpcServer) GetAccounts(ctx context.Context, r *pb.GetAccountsRequest) (*pb.GetAccountsResponse, error) {
+	res, err := s.service.GetAccounts(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	accounts := []*pb.Account{}
+	for _, acct := range res {
+		accounts = append(accounts, &pb.Account{
+			Id: acct.ID,
+			Name: acct.Name,
+		})
+	}
+
+	return &pb.GetAccountsResponse{Account: accounts}, nil
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.24.5
 
 require (
 	github.com/99designs/gqlgen v0.17.78
+	github.com/segmentio/ksuid v1.0.4
 	github.com/vektah/gqlparser/v2 v2.5.30
 )
 
